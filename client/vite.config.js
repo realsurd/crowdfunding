790166842247d0c91ab536c6d2b47a1b7df126a2
Adeyemi Sadiq <adexsadiq01@gmail.com>
2023-11-02 16:22:40 +0100

    last commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/project_crowdfunding",
   plugins: [react()],
   define: {
     global: "globalThis",
